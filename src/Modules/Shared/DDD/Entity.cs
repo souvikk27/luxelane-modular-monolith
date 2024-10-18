b8e694565b8476e6a979cbdb59167f1985865b34
souvikk27 <souvikvip@outlook.com>
2024-10-18 21:17:51 +0530

    introducing shared components

---
+++ src/Modules/Shared/DDD/Entity.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared.DDD;
+
+public abstract class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedBy { get; set; }
+    public DateTime LastModifiedAt { get; set; }
+    public DateTime LastModifiedBy { get; set; }
+
+    protected Entity()
+    {
+    }
+
+    protected Entity(T id)
+    {
+        Id = id;
+    }
+}
---
+++ src/Modules/Shared/DDD/IEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.DDD;
+
+public interface IEntity
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedBy { get; set; }
+
+    public DateTime LastModifiedAt { get; set; }
+    public DateTime LastModifiedBy { get; set; }
+}
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
