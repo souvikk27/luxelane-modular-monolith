57faa967dc227e0ea1615783906e81f753022147
Souvik Kundu <souvikvip@outlook.com>
2024-10-23 17:56:44 +0530

    save changes interceptor implementation

---
+++ src/Modules/Shared/Data/Interceptors/AuditLoggingInterceptor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Shared.Data.Interceptors;
+
+public class AuditLoggingInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData,
+        InterceptionResult<int> result)
+    {
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void UpdateEntities(DbContext? context)
+    {
+
+    }
+}
